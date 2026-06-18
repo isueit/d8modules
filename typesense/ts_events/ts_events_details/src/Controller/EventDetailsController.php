46e8058c5f068b114f7423d22aab5b89295c71f6
Brian Webster <bwebster@iastate.edu>
2026-06-18 14:31:42 -0500

    Redirect ts-events details pages when the event info can't be found (ie event is past)

--- typesense/ts_events/ts_events_details/src/Controller/EventDetailsController.php
+++ typesense/ts_events/ts_events_details/src/Controller/EventDetailsController.php
@@ -2,10 +2,10 @@
 
 namespace Drupal\ts_events_details\Controller;
 
-use Drupal;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\isueo_helpers\ISUEOHelpers;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Exception;
+use Drupal\Core\Routing\TrustedRedirectResponse as RoutingTrustedRedirectResponse;
 
 /**
  * Provides route responses for the ts_events_details  module.
@@ -22,23 +22,29 @@ class EventDetailsController extends ControllerBase
   {
     // Do NOT cache the events details page
     \Drupal::service('page_cache_kill_switch')->trigger();
+    $client = ISUEOHelpers\Typesense::getClient('events');
 
-    $raw = ISUEOHelpers\Typesense::searchCollection('events', '*', '*', '', 10, 1, 'id:' . $eventID);
-    if (!$raw || $raw['found'] != 1) {
-      return [
-        '#title' => $eventTitle,
-        '#markup' => '<p>Event details not found</p>',
-      ];
+    try {
+      $event = $client->collections['events']->documents[$eventID]->retrieve();
+
+      $element = array(
+        '#theme' => 'ts_events_details',
+        '#title' => $event['title'],
+        '#event' => $event,
+        '#attached' => ['library' => ['ts_events_details/ts_events_details']],
+      );
+      return $element;
+    } catch (Exception $ex) {
     }
-    $event = $raw['hits'][0]['document'];
 
-    $element = array(
-      '#theme' => 'ts_events_details',
-      '#title' => $event['title'],
-      '#event' => $event,
-      '#attached' => ['library' => ['ts_events_details/ts_events_details']],
-    );
-    return $element;
+    // If we get here, it means the event wasn't in the events collection
+    try {
+      $past_event = $client->collections['events_programs']->documents[$eventID]->retrieve();
+      $raw = ISUEOHelpers\Typesense::searchCollection('plp_programs', $past_event['Planned_Program__c'], 'field_plp_program_event_pgm_ids');
+      $program = $raw['hits'][0]['document'];
+      return new RoutingTrustedRedirectResponse('https://www.extension.iastate.edu' . $program['url']);
+    } catch (Exception $ex) {
+      return new RoutingTrustedRedirectResponse('https://www.extension.iastate.edu/calendar');
+    }
   }
-
 }
