0be42355317fb63ff1de0f8a4ee941ab51dbb854
Brian Webster <bwebster@iastate.edu>
2026-07-08 17:14:36 -0500

    Added module to do mappings of related to related Salesforce opjects

---
+++ salesforce/sf_fields/src/Plugin/SalesforceMappingField/RelatedTaxonomyField.php
@@ -0,0 +1,240 @@
+<?php
+
+namespace Drupal\sf_fields\Plugin\SalesforceMappingField;
+
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\field\Entity\FieldConfig;
+use Drupal\salesforce\Exception as SalesforceException;
+use Drupal\salesforce\SObject;
+use Drupal\salesforce_mapping\Entity\SalesforceMappingInterface;
+use Drupal\salesforce_mapping\SalesforceMappingFieldPluginBase;
+use Drupal\taxonomy\Entity\Term;
+
+/**
+ * Adapter for entity Reference and fields.
+ *
+ * @Plugin(
+ *   id = "RelatedTaxonomyField",
+ *   label = @Translation("Related Taxonomy Field (dot notation)")
+ * )
+ */
+class RelatedTaxonomyField extends SalesforceMappingFieldPluginBase
+{
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state)
+  {
+    $pluginForm = parent::buildConfigurationForm($form, $form_state);
+
+    // @todo inspecting the form and form_state feels wrong, but haven't found a good way to get the entity from config before the config is saved.
+    $options = $this->getConfigurationOptions($form['#entity']);
+
+    if (empty($options)) {
+      $pluginForm['drupal_field_value'] += [
+        '#markup' => $this->t('No available taxonomy reference fields.'),
+      ];
+    } else {
+      $pluginForm['drupal_field_value'] += [
+        '#type' => 'select',
+        '#options' => $options,
+        '#empty_option' => $this->t('- Select -'),
+        '#default_value' => $this->config('drupal_field_value'),
+        '#description' => $this->t('Select a taxonomy reference field.<br />A taxonomy reference field will be used to sync to the term name.<br />If a term with the given string does not exist one will be created.'),
+      ];
+
+      // Same free-text dot-notation input as the text-field version.
+      $pluginForm['salesforce_field'] = [
+        '#title'         => $this->t('Salesforce related field'),
+        '#type'          => 'textfield',
+        '#description'   => $this->t(
+          'Enter a dot-notation path to a related Salesforce field, e.g. <code>Account.Industry</code> or <code>Opportunity.Type</code>. The value returned will be matched against taxonomy term names.'
+        ),
+        '#default_value' => $this->config('salesforce_field'),
+        '#required'      => TRUE,
+      ];
+    }
+    return $pluginForm;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function value(EntityInterface $entity, SalesforceMappingInterface $mapping)
+  {
+    $field_name = $this->config('drupal_field_value');
+    $instances = $this->entityFieldManager->getFieldDefinitions(
+      $entity->getEntityTypeId(),
+      $entity->bundle()
+    );
+
+    if (empty($instances[$field_name])) {
+      return;
+    }
+
+    $field = $entity->get($field_name);
+    if (empty($field->getValue()) || is_null($field->entity)) {
+      // This reference field is blank or the referenced entity no longer
+      // exists.
+      return;
+    }
+
+    $referencedEntities = [];
+    // Map the term name to the salesforce field.
+    foreach ($field->referencedEntities() as $referencedEntity) {
+      $referencedEntities[] = $referencedEntity->getName();
+    }
+    return $referencedEntities;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function pullValue(SObject $sf_object, EntityInterface $entity, SalesforceMappingInterface $mapping)
+  {
+
+    if (!$this->pull() || empty($this->config('salesforce_field'))) {
+      throw new SalesforceException('No data to pull. Salesforce field mapping is not defined.');
+    }
+
+    $field_name = $this->config('drupal_field_value');
+    $instance = FieldConfig::loadByName($this->mapping->getDrupalEntityType(), $this->mapping->getDrupalBundle(), $field_name);
+    if (empty($instance)) {
+      return;
+    }
+
+    //$value = $sf_object->field($this->config('salesforce_field'));
+    $path = $this->config('salesforce_field');
+    $value = $this->extractValue($sf_object, $path);
+
+    // Empty value means nothing to do here.
+    if (empty($value)) {
+      return NULL;
+    }
+
+    // Get the appropriate vocab from the field settings.
+    $vocabs = $instance->getSetting('handler_settings')['target_bundles'];
+
+    if (empty($vocabs)) {
+      return;
+    }
+
+    // If this is a multivalue field split the value from Salesforce into parts.
+    $field_values = explode(";", $value);
+    $term_ids = [];
+    foreach ($field_values as $field_value) {
+      // Look for a term that matches the string in the salesforce field.
+      $tids = $this->entityTypeManager
+        ->getStorage('taxonomy_term')
+        ->getQuery()
+        ->accessCheck(FALSE)
+        ->condition('vid', $vocabs, 'IN')
+        ->condition('name', $field_value)
+        ->execute();
+      if (!empty($tids)) {
+        $term_ids[] = reset($tids);
+      }
+
+      // If we cant find an existing term, create a new one.
+      if (empty($tids)) {
+        $vocab = reset($vocabs);
+
+        $term = Term::create([
+          'name' => $field_value,
+          'vid' => $vocab,
+        ]);
+        $term->save();
+        $term_ids[] = $term->id();
+      }
+    }
+
+    return $term_ids;
+  }
+
+  /**
+   * Helper to build form options.
+   */
+  private function getConfigurationOptions($mapping)
+  {
+    $instances = $this->entityFieldManager->getFieldDefinitions(
+      $mapping->get('drupal_entity_type'),
+      $mapping->get('drupal_bundle')
+    );
+    $options = [];
+    foreach ($instances as $name => $instance) {
+      $hand = $instance->getSetting('handler');
+      // ???
+      if ($hand != "default:taxonomy_term") {
+        continue;
+      }
+      $options[$name] = $instance->getLabel();
+    }
+    asort($options);
+    return $options;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPluginDefinition()
+  {
+    $definition = parent::getPluginDefinition();
+    // Add reference field.
+    if ($field = FieldConfig::loadByName($this->mapping->getDrupalEntityType(), $this->mapping->getDrupalBundle(), $this->config('drupal_field_value'))) {
+      $definition['config_dependencies']['config'][] = $field->getConfigDependencyName();
+      // Add dependencies of referenced field.
+      foreach ($field->getDependencies() as $type => $dependency) {
+        foreach ($dependency as $item) {
+          $definition['config_dependencies'][$type][] = $item;
+        }
+      }
+    }
+    return $definition;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function checkFieldMappingDependency(array $dependencies)
+  {
+    $definition = $this->getPluginDefinition();
+    foreach ($definition['config_dependencies'] as $type => $dependency) {
+      foreach ($dependency as $item) {
+        if (!empty($dependencies[$type][$item])) {
+          return TRUE;
+        }
+      }
+    }
+    return parent::checkFieldMappingDependency($dependencies);
+  }
+
+  /**
+   * Resolves a dot-notation (or flat) path against the Salesforce object.
+   */
+  protected function extractValue(SObject $sf_object, string $path)
+  {
+    if (strpos($path, '.') === FALSE) {
+      try {
+        return $sf_object->field($path);
+      } catch (\Exception $e) {
+        return NULL;
+      }
+    }
+
+    [$relationship, $field] = explode('.', $path, 2);
+
+    try {
+      $related = $sf_object->field($relationship);
+    } catch (\Exception $e) {
+      return NULL;
+    }
+
+    if (empty($related) || !is_array($related)) {
+      return NULL;
+    }
+
+    return $related[$field] ?? NULL;
+  }
+}
